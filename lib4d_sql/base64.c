8a8e17f72cca39b5f7fb6c8de9a1bb90a6085a83
fourd <fourd@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-08-25 08:35:39 +0000

    update licence message

--- lib4d_sql/base64.c
+++ lib4d_sql/base64.c
@@ -1,20 +1,32 @@
 /*
-   +----------------------------------------------------------------------+
-   | PHP Version 5                                                        |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 1997-2008 The PHP Group                                |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-   | Author: Jim Winstead <jimw@php.net>                                  |
-   +----------------------------------------------------------------------+
- */
+  +----------------------------------------------------------------------+
+  | lib4D_SQL                                                            |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2009 The PHP Group                                     |
+  +----------------------------------------------------------------------+
+  |                                                                      |
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  |                                                                      |
+  | Its original copy is usable under several licenses and is available  |
+  | through the world-wide-web at the following url:                     |
+  | http://freshmeat.net/projects/lib4d_sql                              |
+  |                                                                      |
+  | Unless required by applicable law or agreed to in writing, software  |
+  | distributed under the License is distributed on an "AS IS" BASIS,    |
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      |
+  | implied. See the License for the specific language governing         |
+  | permissions and limitations under the License.                       |
+  +----------------------------------------------------------------------+
+  | Contributed by: 4D <php@4d.fr>, http://www.4d.com                    |
+  |                 Alter Way, http://www.alterway.fr                    |
+  | Authors: Stephane Planquart <stephane.planquart@o4db.com>            |
+  |          Alexandre Morgaut <php@4d.fr>                               |
+  +----------------------------------------------------------------------+
+*/
+
 /* $Id$ */
 
 #include <string.h>
--- lib4d_sql/fourd_result.c
+++ lib4d_sql/fourd_result.c
@@ -1,22 +1,32 @@
-/***********************************************************************
-    (C) Copyright 2009 Stephane Planquart
+/*
+  +----------------------------------------------------------------------+
+  | lib4D_SQL                                                            |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 2009 The PHP Group                                     |
+  +----------------------------------------------------------------------+
+  |                                                                      |
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  |                                                                      |
+  | Its original copy is usable under several licenses and is available  |
+  | through the world-wide-web at the following url:                     |
+  | http://freshmeat.net/projects/lib4d_sql                              |
+  |                                                                      |
+  | Unless required by applicable law or agreed to in writing, software  |
+  | distributed under the License is distributed on an "AS IS" BASIS,    |
+  | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or      |
+  | implied. See the License for the specific language governing         |
+  | permissions and limitations under the License.                       |
+  +----------------------------------------------------------------------+
+  | Contributed by: 4D <php@4d.fr>, http://www.4d.com                    |
+  |                 Alter Way, http://www.alterway.fr                    |
+  | Authors: Stephane Planquart <stephane.planquart@o4db.com>            |
+  |          Alexandre Morgaut <php@4d.fr>                               |
+  +----------------------------------------------------------------------+
+*/
 
-    These sources is free software. They are published under any of 
-    the following licences : 
-    APACHE
-    BSD
-    GNU
-    LGPL
-    PHP
-
-    These sources is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY. Without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-    You may contact the author by:
-       e-mail:  splanquart@php.net
-       e-mail:  fourd@php.net
-*************************************************************************/
 #include "fourd.h"
 #include "fourd_int.h"
 FOURD_LONG8 fourd_num_rows(FOURD_RESULT *result)
